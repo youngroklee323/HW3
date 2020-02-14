06894b5becc5298dffc51f7ac329f6923fb1b9ed
youngroklee323 <60499476+youngroklee323@users.noreply.github.com>
2020-02-14 13:46:26 -0800

    Create height-lawnmower.js

---
+++ height-lawnmower.js
@@ -0,0 +1,35 @@
+function setup() {
+  createCanvas(400, 200);
+  colorMode(HSB);
+  
+}
+
+var x = 0;
+var h = 10;
+
+function draw() {
+  stroke(random(60, 70), 100, 90);
+  line(x, height-10, x+random(-10, 10), height-10-random(h));
+  noStroke();
+  
+  //where the grass resets.
+  fill(240);
+  noStroke();
+  rect(0,0,width,50);
+
+  x = x + 10;
+
+  if (x > width) {
+    x = random(10);
+    h = h + 3;
+  }
+// lawnowner comes whe the grass reaches at the specific height
+  if (h > 150) {
+    fill(255);
+    rect(0, 0, width, height-15);
+    h = 10;
+  }
+
+  fill(40, 100, 60);
+  rect(0, height-10, width, 10);
+}
